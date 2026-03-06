283da8ab13c3d0f04377a3e194adb3a280559c25
littlephillips <phillipkinuthia72@gmail.com>
2026-03-06 06:16:15 +0300

    updated the allinevents project image

--- src/pages/Project/ProjectPage.js
+++ src/pages/Project/ProjectPage.js
@@ -2,14 +2,14 @@ import React from 'react';
 import '../../styles/project.css';
 
 const images = [
-  { src: '/assets/img/cashdvance.png',        alt: 'cashdvance', link: 'https://cash-advance-72.web.app', name: 'Cash Advance' },
+  { src: '/assets/img/allInvents.png',        alt: 'allInvents',      link: 'https://allineventsclient.web.app/',    name: 'AllInEvents' },  
+  { src: '/assets/img/allInvents_crm.png',    alt: 'allInvents_crm',  link: 'https://allinevents-6be19.web.app/crm', name: 'AllInEvents CRM' },
+  { src: '/assets/img/cashdvance.png',        alt: 'cashdvance',      link: 'https://cash-advance-72.web.app',       name: 'Cash Advance' },
   { src: '/assets/img/GlobalMarketIndex.png', alt: 'MajorMarketIndecesAnalyzer', link: 'https://github.com/littlephillips/tradingeconomics/tree/master/Global-Market-Index', name: 'Major Market Index Analyzer' },
-  { src: '/assets/img/allInvents.png',       alt: 'allInvents', link: 'https://allineventsclient.web.app/', name: 'AllInEvents' },  
-  { src: '/assets/img/allInvents_crm.png',   alt: 'allInvents_crm', link: 'https://allinevents-6be19.web.app/crm', name: 'AllInEvents CRM' }, 
-  { src: '/assets/img/ContactManager.png',   alt: 'ContactManager', link: 'https://littlephillips.github.io/Contact-Web-Application/', name: 'Contact Manager' },
-  { src: '/assets/img/catGenerator.png',     alt: 'catGenerator', link: 'https://littlephillips.github.io/generate-random-cat-pictures/', name: 'Cat Generator' },
-  { src: '/assets/img/soon.webp',            alt: 'Project image 5', link: '#', name: '' },
-  { src: '/assets/img/soon.webp',            alt: 'Project image 6', link: '#', name: '' },
+  { src: '/assets/img/ContactManager.png',    alt: 'ContactManager',  link: 'https://littlephillips.github.io/Contact-Web-Application/', name: 'Contact Manager' },
+  { src: '/assets/img/catGenerator.png',      alt: 'catGenerator',    link: 'https://littlephillips.github.io/generate-random-cat-pictures/', name: 'Cat Generator' },
+  { src: '/assets/img/soon.webp',             alt: 'Project image 5', link: '#', name: '' },
+  { src: '/assets/img/soon.webp',           alt: 'Project image 6',   link: '#', name: '' },
 ];
 
 const ProjectPage = () => {
